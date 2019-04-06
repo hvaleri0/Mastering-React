366b2ccf8f4a21c9c77eb955751901afcdc17e7a
Hector Valerio <hector.valerio@gmail.com>
2019-04-05 20:07:16 -0400

    Arrow Functions

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,11 +1,23 @@
-const person = {
-  name: "Mosh",
-  walk() {
-    console.log(this);
-  }
+const square = function(number) {
+  return number * number;
 };
 
-person.walk();
+const square2 = number => number * number;
 
-const walk = person.walk.bind(person);
-walk();
+console.log(square(5));
+console.log(square2(5));
+
+const jobs = [
+  { id: 1, isActive: true },
+  { id: 2, isActive: true },
+  { id: 3, isActive: false }
+];
+
+const activeJobs = jobs.filter(function(job) {
+  return job.isActive;
+});
+
+const activeJobsR = jobs.filter(job => job.isActive);
+
+console.log(activeJobs);
+console.log(activeJobsR);
