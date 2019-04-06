6915410ffd1a5553930b3127d962c4352c71a9c6
Hector Valerio <hector.valerio@gmail.com>
2019-04-05 20:50:56 -0400

    Object Destructuring

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,6 +1,13 @@
-const colors = ["red", "green", "blue"];
-// const items = colors.map(function(color) {
-//   return "<li>" + color + "</li>";
-// });
-const items = colors.map(color => `<li>${color}</li>`);
-console.log(items);
+const address = {
+  street: "",
+  city: "",
+  country: ""
+};
+
+// const street = address.street;
+// const city = address.city;
+// const country = address.country;
+
+const { street, city, country } = address; //same as above !
+
+const { street: st } = address; // same as const st = address.st
