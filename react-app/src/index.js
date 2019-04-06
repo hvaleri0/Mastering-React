cbf31969d48790a0877df6c6985069df11b12e6d
Hector Valerio <hector.valerio@gmail.com>
2019-04-05 21:26:46 -0400

    Inheritance

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -7,6 +7,18 @@ class Person {
   }
 }
 
+class Teacher extends Person {
+  constructor(name, degree) {
+    super(name);
+    this.degree = degree;
+  }
+  teach() {
+    console.log("teach");
+  }
+}
+
 const person = new Person("Hector");
+const teacher = new Teacher("Hector", "programming");
 
 console.log(person);
+console.log(teacher);
