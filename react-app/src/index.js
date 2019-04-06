d43b02096b6b0974d0ba7d94af04f2e40b4a0a9f
Hector Valerio <hector.valerio@gmail.com>
2019-04-05 21:39:06 -0400

    Modules

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,21 +1,5 @@
-class Person {
-  constructor(name) {
-    this.name = name;
-  }
-  walk() {
-    console.log("walk");
-  }
-}
-
-class Teacher extends Person {
-  constructor(name, degree) {
-    super(name);
-    this.degree = degree;
-  }
-  teach() {
-    console.log("teach");
-  }
-}
+import { Teacher } from "./teacher";
+import { Person } from "./person";
 
 const person = new Person("Hector");
 const teacher = new Teacher("Hector", "programming");
