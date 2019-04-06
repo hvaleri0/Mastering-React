8be22fa29463bddf6b0c0750df8d82706ac95c5b
Hector Valerio <hector.valerio@gmail.com>
2019-04-05 20:40:18 -0400

    array.map Method

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -1,25 +1,6 @@
-const person = {
-  talk() {
-    setTimeout(function() {
-      console.log("this", this);
-    }, 1000);
-  }
-};
-person.talk(); // returns the window object Wrong we want the Person Object
-
-const person1 = {
-  talk() {
-    const self = this;
-    setTimeout(function() {
-      console.log("self", self);
-    }, 1000);
-  }
-};
-person1.talk(); // old school way, assign self to this
-
-const person2 = {
-  talk() {
-    setTimeout(() => console.log("this", this), 1000);
-  }
-};
-person2.talk(); // The Cleanest way! arrow functions dont rebind the this
+const colors = ["red", "green", "blue"];
+// const items = colors.map(function(color) {
+//   return "<li>" + color + "</li>";
+// });
+const items = colors.map(color => `<li>${color}</li>`);
+console.log(items);
