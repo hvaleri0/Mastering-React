ed294074af51fdacb82f3910b6ad19487a25a33c
Hector Valerio <hector.valerio@gmail.com>
2019-04-19 11:22:56 -0400

    Destructuring Arguments

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 //Stateless Functional Component
-const NavBar = props => {
+const NavBar = ({ totalCounters }) => {
   return (
     <nav className="navbar navbar-light bg-light">
       <a className="navbar-brand" href="#">
         Navbar{" "}
-        <span className="badge-pill badge-secondary">
-          {props.totalCounters}
-        </span>
+        <span className="badge-pill badge-secondary">{totalCounters}</span>
       </a>
     </nav>
   );
