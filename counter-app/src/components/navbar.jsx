34fdb50ebbf94f919d024e9eb6803191a54f1540
Hector Valerio <hector.valerio@gmail.com>
2019-04-19 12:26:41 -0400

    Lifecycle Hooks - Mounting Phase

--- counter-app/src/components/navbar.jsx
+++ counter-app/src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 //Stateless Functional Component
 const NavBar = ({ totalCounters }) => {
+  console.log("NavBar-Rendered");
   return (
     <nav className="navbar navbar-light bg-light">
-      <a className="navbar-brand" href="#">
+      <a className="navbar-brand" href="navbar1">
         Navbar{" "}
         <span className="badge-pill badge-secondary">{totalCounters}</span>
       </a>
