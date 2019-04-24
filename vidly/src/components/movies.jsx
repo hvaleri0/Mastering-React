190ae5c3dd07ace85550ef224241c6c909fb5964
Hector Valerio <hector.valerio@gmail.com>
2019-04-23 20:00:54 -0400

    Sorting - Implementing Sorting

--- vidly/src/components/movies.jsx
+++ vidly/src/components/movies.jsx
@@ -5,13 +5,15 @@ import ListGroup from "./common/listGroup";
 import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
+import _ from "lodash";
 
 class Movies extends Component {
   state = {
     movies: [],
     genres: [],
     currentPage: 1,
-    pageSize: 4
+    pageSize: 4,
+    sortColumn: { path: "title", order: "asc" }
   };
 
   componentDidMount() {
@@ -42,13 +44,21 @@ class Movies extends Component {
   };
 
   handleSort = path => {
-    console.log(path);
+    const sortColumn = { ...this.state.sortColumn };
+    if (sortColumn.path === path) {
+      sortColumn.order = sortColumn.order === "asc" ? "desc" : "asc";
+    } else {
+      sortColumn.path = path;
+      sortColumn.order = "asc";
+    }
+    this.setState({ sortColumn });
   };
 
   render() {
     const { length: count } = this.state.movies;
     const {
       currentPage,
+      sortColumn,
       pageSize,
       selectedGenre,
       movies: allMovies
@@ -63,7 +73,9 @@ class Movies extends Component {
         ? allMovies.filter(m => m.genre._id === selectedGenre._id)
         : allMovies;
 
-    const movies = paginate(filtered, currentPage, pageSize);
+    const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
+
+    const movies = paginate(sorted, currentPage, pageSize);
 
     return (
       <div className="row">
