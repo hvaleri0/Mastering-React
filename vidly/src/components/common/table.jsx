4cd3f6747d53e50f4452734384065703aae13ca6
Hector Valerio <hector.valerio@gmail.com>
2019-04-25 13:42:18 -0400

    Sorting - Extracting a Method & Destructuring Args

--- vidly/src/components/common/table.jsx
+++ vidly/src/components/common/table.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import TableHeader from "./tableHeader";
 import TableBody from "./tableBody";
 
-const Table = props => {
-  const { data, onSort, sortColumn, columns } = props;
-
+const Table = ({ data, onSort, sortColumn, columns }) => {
   return (
     <table className="table">
       <TableHeader columns={columns} sortColumn={sortColumn} onSort={onSort} />
