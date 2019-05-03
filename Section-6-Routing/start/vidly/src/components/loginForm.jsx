4c45fd69c89ef622b6801c583f88fe4934c95c6a
Hector Valerio <hector.valerio@gmail.com>
2019-05-03 18:22:54 -0400

    handling Multiple Inputs

--- Section-6-Routing/start/vidly/src/components/loginForm.jsx
+++ Section-6-Routing/start/vidly/src/components/loginForm.jsx
@@ -5,9 +5,9 @@ class LoginForm extends Component {
     account: { username: "", password: "" }
   };
 
-  handleChange = e => {
+  handleChange = ({ currentTarget: input }) => {
     const account = { ...this.state.account };
-    account.username = e.currentTarget.value;
+    account[input.name] = input.value;
     this.setState({ account });
   };
 
@@ -19,6 +19,7 @@ class LoginForm extends Component {
   };
 
   render() {
+    const { account } = this.state;
     return (
       <div>
         <h1>Login</h1>
@@ -26,16 +27,24 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              value={this.state.account.username}
+              value={account.username}
               onChange={this.handleChange}
               id="username"
+              name="username"
               type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <input id="password" type="text" className="form-control" />
+            <input
+              value={account.password}
+              onChange={this.handleChange}
+              id="password"
+              name="password"
+              type="text"
+              className="form-control"
+            />
           </div>
           <button className="btn btn-primary">Login</button>
         </form>
