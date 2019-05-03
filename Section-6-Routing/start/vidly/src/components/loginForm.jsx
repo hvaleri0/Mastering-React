f48b33a0620da7e1bfd61a46050135a4757257ad
Hector Valerio <hector.valerio@gmail.com>
2019-05-03 18:12:19 -0400

    Controlled Elements

--- Section-6-Routing/start/vidly/src/components/loginForm.jsx
+++ Section-6-Routing/start/vidly/src/components/loginForm.jsx
@@ -1,18 +1,21 @@
 import React, { Component } from "react";
 
 class LoginForm extends Component {
-  username = React.createRef();
+  state = {
+    account: { username: "", password: "" }
+  };
 
-  // componentDidMount() {
-  //   this.username.current.focus();
-  // }
+  handleChange = e => {
+    const account = { ...this.state.account };
+    account.username = e.currentTarget.value;
+    this.setState({ account });
+  };
 
   handleSubmit = e => {
     e.preventDefault();
 
     //call the server
-    const username = this.username.current.value;
-    console.log(username);
+    console.log("username");
   };
 
   render() {
@@ -23,8 +26,8 @@ class LoginForm extends Component {
           <div className="form-group">
             <label htmlFor="username">Username</label>
             <input
-              autoFocus
-              ref={this.username}
+              value={this.state.account.username}
+              onChange={this.handleChange}
               id="username"
               type="text"
               className="form-control"
