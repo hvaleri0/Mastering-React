e0fbe07712c2be847f747ae906875e1d50800094
Hector Valerio <hector.valerio@gmail.com>
2019-05-10 18:58:33 -0400

    Extracting A Reusable HTTP Service

--- Section-8-Backend/start/http-app/src/App.js
+++ Section-8-Backend/start/http-app/src/App.js
@@ -1,21 +1,6 @@
 import React, { Component } from "react";
-import axios from "axios";
+import http from "./services/httpservice";
 import "./App.css";
-
-axios.interceptors.response.use(null, error => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("logging the error", error);
-
-    alert("an unexpected error occured.");
-  }
-
-  return Promise.reject(error);
-});
 
 const apiEndpoint = "http://jsonplaceholder.typicode.com/posts";
 
@@ -25,13 +10,13 @@ class App extends Component {
   };
 
   async componentDidMount() {
-    const { data: posts } = await axios.get(apiEndpoint);
+    const { data: posts } = await http.get(apiEndpoint);
     this.setState({ posts });
   }
 
   handleAdd = async () => {
     const obj = { title: "a", body: "b" };
-    const { data: post } = await axios.post(apiEndpoint, obj);
+    const { data: post } = await http.post(apiEndpoint, obj);
 
     const posts = [post, ...this.state.posts];
     this.setState({ posts });
@@ -39,7 +24,7 @@ class App extends Component {
 
   handleUpdate = async post => {
     post.title = "Updated";
-    await axios.put(`${apiEndpoint}/${post.id}`, post);
+    await http.put(`${apiEndpoint}/${post.id}`, post);
 
     const posts = [...this.state.posts];
     const index = posts.indexOf(post);
@@ -54,9 +39,9 @@ class App extends Component {
     this.setState({ posts });
 
     try {
-      //await axios.delete(`${apiEndpoint}s/${post.id}`); //expected error
-      //await axios.delete(`s${apiEndpoint}/${post.id}`); // unexpected error
-      await axios.delete(`${apiEndpoint}/${post.id}`); //normal
+      //await http.delete(`${apiEndpoint}s/${post.id}`); //expected error
+      //await http.delete(`s${apiEndpoint}/${post.id}`); // unexpected error
+      await http.delete(`${apiEndpoint}/${post.id}`); //normal
     } catch (ex) {
       console.log("HANDLE DELETE CATCH BLOCK");
       //Expected (404: not found, 400: bad request) - CLIENT ERRORS
