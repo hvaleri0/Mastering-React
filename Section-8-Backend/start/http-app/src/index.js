b71a6ef18b211a0989bd802ee822e1ceeda12b22
Hector Valerio <hector.valerio@gmail.com>
2019-05-10 22:18:54 -0400

    Extracting a Logger Service

--- Section-8-Backend/start/http-app/src/index.js
+++ Section-8-Backend/start/http-app/src/index.js
@@ -2,13 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import * as Sentry from "@sentry/browser";
+import logger from "./services/logservice";
 import "./index.css";
 import "bootstrap/dist/css/bootstrap.css";
 
-Sentry.init({
-  dsn: "https://561163c459234807bdc9da2c0e43081b@sentry.io/1457238"
-});
+logger.init();
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
