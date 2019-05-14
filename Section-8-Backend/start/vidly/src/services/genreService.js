e25e186334f6b96ffa34123fbee40e42857bfd02
Hector Valerio <hector.valerio@gmail.com>
2019-05-13 21:59:04 -0400

    Exercise - Extracting a Config File in vidly

--- Section-8-Backend/start/vidly/src/services/genreService.js
+++ Section-8-Backend/start/vidly/src/services/genreService.js
@@ -1,6 +1,6 @@
 import http from "./httpservice";
-import config from "../config.json";
+import { apiUrl } from "../config.json";
 
 export function getGenres() {
-  return http.get(`${config.apiEndpoint}genres`);
+  return http.get(`${apiUrl}/genres`);
 }
--- Section-8-Backend/start/vidly/src/services/movieService.js
+++ Section-8-Backend/start/vidly/src/services/movieService.js
@@ -1,10 +1,10 @@
 import http from "./httpservice";
-import config from "../config.json";
+import { apiUrl } from "../config.json";
 
 export function getMovies() {
-  return http.get(`${config.apiEndpoint}movies`);
+  return http.get(`${apiUrl}/movies`);
 }
 
 export function deleteMovie(movieId) {
-  return http.delete(`${config.apiEndpoint}movies/${movieId}`);
+  return http.delete(`${apiUrl}/movies/${movieId}`);
 }
