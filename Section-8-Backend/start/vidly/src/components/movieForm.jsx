3932669374f4a15e39b43f9f351472738c3fadc5
Hector Valerio <hector.valerio@gmail.com>
2019-05-13 22:21:47 -0400

    Exercise - Connect Movie Form to the vidly BE

--- Section-8-Backend/start/vidly/src/components/movieForm.jsx
+++ Section-8-Backend/start/vidly/src/components/movieForm.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Joi from "joi-browser";
 import Form from "./common/form";
-import { getMovie, saveMovie } from "../services/fakeMovieService";
+import { getMovie, saveMovie } from "../services/movieService";
 import { getGenres } from "../services/genreService";
 
 class MovieForm extends Form {
@@ -36,17 +36,21 @@ class MovieForm extends Form {
       .label("Daily Rental Rate")
   };
 
-  componentDidMount() {
-    const genres = getGenres();
+  async componentDidMount() {
+    const { data: genres } = await getGenres();
     this.setState({ genres });
 
     const movieId = this.props.match.params.id;
     if (movieId === "new") return;
 
-    const movie = getMovie(movieId);
-    if (!movie) return this.props.history.replace("/not-found");
-
-    this.setState({ data: this.mapToViewModel(movie) });
+    try {
+      const { data: movie } = await getMovie(movieId);
+      this.setState({ data: this.mapToViewModel(movie) });
+    } catch (ex) {
+      console.log("Catch", ex);
+      if (ex.response && ex.response.status === 404)
+        this.props.history.replace("/not-found");
+    }
   }
 
   mapToViewModel(movie) {
