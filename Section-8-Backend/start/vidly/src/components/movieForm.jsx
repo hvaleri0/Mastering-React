ae54b72582b0822fb28d66e6640c98097cedafaf
Hector Valerio <hector.valerio@gmail.com>
2019-05-13 22:31:02 -0400

    Refactoring

--- Section-8-Backend/start/vidly/src/components/movieForm.jsx
+++ Section-8-Backend/start/vidly/src/components/movieForm.jsx
@@ -36,14 +36,15 @@ class MovieForm extends Form {
       .label("Daily Rental Rate")
   };
 
-  async componentDidMount() {
+  async populateGenre() {
     const { data: genres } = await getGenres();
     this.setState({ genres });
+  }
 
-    const movieId = this.props.match.params.id;
-    if (movieId === "new") return;
-
+  async populateMovie() {
     try {
+      const movieId = this.props.match.params.id;
+      if (movieId === "new") return;
       const { data: movie } = await getMovie(movieId);
       this.setState({ data: this.mapToViewModel(movie) });
     } catch (ex) {
@@ -51,6 +52,11 @@ class MovieForm extends Form {
       if (ex.response && ex.response.status === 404)
         this.props.history.replace("/not-found");
     }
+  }
+
+  async componentDidMount() {
+    await this.populateGenre();
+    await this.populateMovie();
   }
 
   mapToViewModel(movie) {
