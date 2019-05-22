7d21f816b92d0a991e7c7e2e61aba0d181e657ac
Hector Valerio <hector.valerio@gmail.com>
2019-05-21 21:12:23 -0400

    test

--- Section-8-Backend/start/vidly/src/index.js
+++ Section-8-Backend/start/vidly/src/index.js
@@ -3,11 +3,10 @@ import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 
-console.log(process.env);
+console.log("SUPERMAN", process.env.REACT_APP_NAME);
 
 ReactDOM.render(
   <BrowserRouter>
@@ -15,4 +14,3 @@ ReactDOM.render(
   </BrowserRouter>,
   document.getElementById("root")
 );
-registerServiceWorker();
